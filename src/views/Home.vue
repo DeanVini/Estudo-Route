244cb288cf1f6bc7f5deb163f75f3f2c614d8150
Dean Vinicius Palmeira de Meneses <dean.meneses@PGMARACAJU.SE.GOV.BR>
2023-06-28 17:17:28 -0300

    functional home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="ele-ta-sem-zap">
     <div  class="destinaion-container">
-            <router-link v-for="destination in destinations" :key="destination.id" :to="`/${destination.slug}`">
-                <div class="infos-container">
-                    <img src="../../public/images/brazil.jpg" alt="">
+            <router-link v-for="(destination) in destinations" :key="destination.id" :to="`/${destination.slug}`">
+                <div class= "infos-container">
+                    <img :src="`../../images/${destination.image}`" alt="">
                     <div class="infos">
                         <h2>{{ destination.name }}</h2>
                         <p>{{ destination.description }}</p>   
@@ -38,19 +38,29 @@ async function getDestinations(){
     background-color: #ffffff;
     white-space: normal;
     flex-direction: column;
+    gap: 40px;
 }
 
 .destinaion-container img{
     border-radius: 4px;
     border: 3px solid #ffffff;
+    
 }
 
 .ele-ta-sem-zap{
 }
 
 .destinaion-container .infos-container{
+    display: flex;
     flex-direction: row !important;
     box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+    transition: 0.13s;
+}
+
+.destinaion-container .infos-container:hover{
+   border-bottom: 3px solid #a5a5a5;
+   border-left: 3px solid #cfcfcf;
+   border-radius: 4px;
 }
 </style>
 
